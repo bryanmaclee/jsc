1fb9a56f4ecbe4578fd8cc99d044edba171622ef
Bryan Maclee <bryanmaclee@gmail.com>
2025-12-18 16:26:47 -0700

    baller

--- dep/elements.js
+++ dep/elements.js
@@ -3,8 +3,9 @@ import { elementTypes } from "./syntax.js";
 const els = new Set(elementTypes.map((el) => el.elType));
 
 export function buildElement(exp) {
-   exp.pop();
-   exp.shift();
+   const shift = exp[0].value === "<" ? 1 : 0;
+   const pop = exp[exp.length - 1].value === ">" ? -1 : undefined;
+   exp = exp.slice(shift, pop);
    return {
       type: "element",
       kind: exp[0].value,
