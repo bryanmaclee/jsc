35c5f2309462cce939729896c5a30e857c1978fa
bryan maclee <bryanmaclee@gmail.com>
2025-12-24 06:52:03 -0700

    switch

--- index.js
+++ index.js
@@ -32,25 +32,35 @@ process.on("exit", (code) => {
 
 async function main() {
    // await startLogger();
-   const datastr = await Bun.file(Files.testFile()).text();
-   const data = truncateInput(datastr);
+   // const datastr = await Bun.file(Files.testFile()).text();
+   const dataStr = await Bun.file(Files.testFile());
+   const byts = await dataStr.bytes();
+   let theStr = "";
+   // for (const al of byts) {
+   //    theStr += String.fromCharCode(al);
+   // }
+   theStr += String.fromCharCode(...byts);
+   console.log(theStr);
+}
+function continueStuff() {
+   // const data = truncateInput(datastr);
    // console.log(data);
    // const lexed = tokenize(data);
-   tokenize(data);
-   await Bun.write(Files.outputText, JSON.stringify(access.tokens, null, 2));
-   // const woWhite = lexed.filter((thing) => thing.kind !== "format");
-   access.trunc = access.tokens.filter((thing) => thing.kind !== "format");
-   await Bun.write(Files.outputTrunk, JSON.stringify(access.trunc, null, 2));
-   let program = instanciateProgram(access.tokens, globalEnv);
-   // console.trace();
-   const out = output(program);
-   const opt = outOpt(program);
-   // console.log(out);
-   await Bun.write(Files.programFile, out);
-   await Bun.write(Files.optimizedFile, opt);
-   // const progOut = preStringify(program);
-   // typeChecker(program);
-   await Bun.write(Files.outputFile, JSON.stringify(program, null, 2));
+   // tokenize(data);
+   // await Bun.write(Files.outputText, JSON.stringify(access.tokens, null, 2));
+   // // const woWhite = lexed.filter((thing) => thing.kind !== "format");
+   // access.trunc = access.tokens.filter((thing) => thing.kind !== "format");
+   // await Bun.write(Files.outputTrunk, JSON.stringify(access.trunc, null, 2));
+   // let program = instanciateProgram(access.tokens, globalEnv);
+   // // console.trace();
+   // const out = output(program);
+   // const opt = outOpt(program);
+   // // console.log(out);
+   // await Bun.write(Files.programFile, out);
+   // await Bun.write(Files.optimizedFile, opt);
+   // // const progOut = preStringify(program);
+   // // typeChecker(program);
+   // await Bun.write(Files.outputFile, JSON.stringify(program, null, 2));
    // console.log(globalEnv);
    // await Bun.write(Files.outputFile, JSON.stringify(progOut, null, 2));
 }
